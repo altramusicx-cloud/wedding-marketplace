6052fad90c6a0e581183ee4716061acdfbdc142f
rhioe <rhioe46@gmail.com>
2026-02-01 20:26:09 +0800

    chore: complete project cleanup - optimized structure

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -3,3 +3,12 @@ export { cn } from './cn'
 export { formatCurrency } from './format-currency'
 export { formatDate, formatDateTime } from './format-date'
 // ... lainnya
+// From lib/utils.ts
+// lib\utils.ts
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
